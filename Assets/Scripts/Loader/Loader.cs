2f462a11e436fd9a508d6c379b16a764160b8602
caleb <66700303+caleb-adeyemo@users.noreply.github.com>
2024-05-02 13:39:31 +0100

    Updated the ManMenu

---
+++ Assets/Scripts/Loader/Loader.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader{
+
+    public enum Scene{
+        Menu,
+        LoadingScene,
+        GamePlay
+    }
+    private static Scene targetScene;
+
+    public static void Load(Scene _targetScene){
+        targetScene = _targetScene;
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+    }
+
+    public static void LoaderCallBack(){
+        SceneManager.LoadScene(targetScene.ToString());
+    }
+}
---
+++ Assets/Scripts/Loader/LoaderCallback.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+    private bool isFristUpdate = true;
+
+    private void Update(){
+        if(isFristUpdate){
+            isFristUpdate = false;
+
+            Loader.LoaderCallBack();
+        }
+    }
+}
