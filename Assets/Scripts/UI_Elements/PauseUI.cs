afcaf7b7c0db6313ee073fb6de6508fd98fb5698
caleb <66700303+caleb-adeyemo@users.noreply.github.com>
2024-05-02 16:06:43 +0100

    fixed issue with pause button not working
    EventSystem GameObject was accidentally deleted

--- Assets/Scripts/UI_Elements/PauseUI.cs
+++ Assets/Scripts/UI_Elements/PauseUI.cs
@@ -3,18 +3,20 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PauseUI : MonoBehaviour{
-
     [SerializeField] private Button resumeButton;
     [SerializeField] private Button menuButton;
 
+
     private void Awake(){
-        resumeButton.onClick.AddListener(()=>{
+        resumeButton.onClick.AddListener( ()=>{
             GameManager.Instance.togglePauseGame();
         });
 
         menuButton.onClick.AddListener(()=>{
             Loader.Load(Loader.Scene.Menu);
         });
+       
+
     }
 
     private void Start(){
@@ -37,5 +39,13 @@ public class PauseUI : MonoBehaviour{
 
     private void hide(){
         gameObject.SetActive(false);
+    }
+
+    private void testFunc(){
+        Debug.Log("Thsi is a test for the Resume Button");
+    }
+
+    private void testFunc2(){
+        Debug.Log("Thsi is a test for the Menu Button");
     }
 }
